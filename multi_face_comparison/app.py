0e985a78d70731e8a647ad70299ff9e18f2abb4c
DESKTOP-UJP2SFI\zhangbin <zhangqiyi1@163.com>
2022-10-12 10:48:11 +0800

    1

---
+++ multi_face_comparison/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, send_from_directory, Response
+from flask import jsonify
+import json
+import re
+import os
+import scipy.misc
+import warnings
+import sys
+import compareFace
+import time
+import detectFace
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/compare', methods=['POST'])
+def compare_faces():
+    target = request.files['target']
+    faces = request.files.getlist("faces")
+    target_filename = secure_filename(target.filename)
+    response = []
+    for face in faces:
+        start = time.time()
+        distance, result = compareFace.main(target, face)
+        distance = float(distance)
+        end = time.time()
+        json_contect = {
+            'result': str(result),
+            'distance': round(distance, 2),
+            'time_taken': round(end - start, 3),
+            'target': target_filename,
+            'face': secure_filename(face.filename)
+        }
+        response.append(json_contect)
+    python2json = json.dumps(response)
+    return app.response_class(python2json, content_type='application/json')
+
+
+@app.route('/api/v1/detect', methods=['POST'])
+def detect_faces():
+    faces = request.files.getlist("faces")
+    # target_filename=secure_filename(target.filename)
+    response = []
+    for face in faces:
+        start = time.time()
+        _, result = detectFace.get_coordinates(face)
+        end = time.time()
+        json_contect = {
+            'coordinates': result,
+            'time_taken': round(end - start, 3),
+            'image_name': secure_filename(face.filename)
+        }
+        response.append(json_contect)
+    python2json = json.dumps(response)
+    return app.response_class(python2json, content_type='application/json')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
---
+++ multi_face_comparison/compareFace.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+import os
+import re
+import scipy.misc
+import warnings
+import face_recognition.api as face_recognition
+import sys
+
+
+def scanKnownPeople(knownPeopleImage):
+    known_names = []
+    known_face_encodings = []
+
+    basename = knownPeopleImage
+    img = face_recognition.load_image_file(knownPeopleImage)
+    encodings = face_recognition.face_encodings(img)
+    if len(encodings) == 1:
+        known_names.append(basename)
+        known_face_encodings.append(encodings[0])
+    return known_names, known_face_encodings
+
+
+def checkImage(image_to_check, known_names, known_face_encodings):
+    unknown_image = face_recognition.load_image_file(image_to_check)
+    # Scale down image if it's giant so things run a little faster
+    if unknown_image.shape[1] > 1600:
+        scale_factor = 1600.0 / unknown_image.shape[1]
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            unknown_image = scipy.misc.imresize(unknown_image, scale_factor)
+
+    unknown_encodings = face_recognition.face_encodings(unknown_image)
+    if len(unknown_encodings) == 1:
+        for unknown_encoding in unknown_encodings:
+            result = face_recognition.compare_faces(known_face_encodings, unknown_encoding)
+            distance = face_recognition.face_distance(known_face_encodings, unknown_encoding)
+            # print(distance[0])
+            # print("True") if True in result else print("False ")
+
+        return distance[0], result[0]
+    else:
+        return "0", "Many Faces or No Faces"
+
+
+def image_files_in_folder(folder):
+    return [os.path.join(folder, f) for f in os.listdir(folder) if re.match(r'.*\.(jpg|jpeg|png)', f, flags=re.I)]
+
+
+def main(known_people_folder, image_to_check):
+    known_names, known_face_encodings = scanKnownPeople(known_people_folder)
+    distance, result = checkImage(image_to_check, known_names, known_face_encodings)
+    return distance, result
+
+
+if __name__ == "__main__":
+    distance, result = main(sys.argv[1], sys.argv[2])
+    print(distance, result)
