5cd1366136312b754178821e1d335903ee7c7f54
zhangqiyi1@163.com <z1980415>
2022-10-15 11:14:49 +0800

    1

--- handTracking/HandTrackingMain.py
+++ handTracking/HandTrackingMain.py
@@ -2,9 +2,20 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0);
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Image",img)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms)
+
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
