c1234907dce7a9c83c0c0a2ce13b338ad7432f9c
DESKTOP-UJP2SFI\zhangbin <zhangqiyi1@163.com>
2022-10-17 17:15:09 +0800

    1

--- Advance_Computer_Vision/chapter-3-face-detection/Basics.py
+++ Advance_Computer_Vision/chapter-3-face-detection/Basics.py
@@ -25,10 +25,18 @@ while True:
             # print(id, detection)
             # print(detection.score)
             # print(detection.location_data.relative_bounding_box)
+            # 检测到的人脸的集合，其中每个人脸都表示为一个检测原始消息，其中包含 人脸的概率、
+            # 1 个边界框、6 个关键点（右眼、左眼、鼻尖、嘴巴中心、右耳、左耳）。
+            # 边界框由 xmin 和 width (由图像宽度归一化为 [0, 1])以及 ymin 和
+            # height (由图像高度归一化为 [0, 1])组成。每个关键点由 x 和 y 组成，
+            # 分别通过图像宽度和高度归一化为 [0, 1]
+
             bboxC = detection.location_data.relative_bounding_box
             ih, iw, ic = img.shape
+
+            # 将边界框的坐标点从比例坐标转换成像素坐标
+            # 将边界框的宽和高从比例长度转换为像素长度
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
-            print(bbox)
             cv2.rectangle(img, bbox, (255, 0, 255), 2)
             cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,
                         2, (255, 0, 255), 2)
